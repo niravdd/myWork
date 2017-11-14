60866a7fae1ecc81e534c7b3a17c140e8c60b742
Maxwell <braxwell@60f81dba73f0.ant.amazon.com>
2017-11-14 21:00:11 +1100

    Minor additions 14th Nov

--- kinesis-datagen/deploy.sh
+++ kinesis-datagen/deploy.sh
@@ -1,6 +1,6 @@
 # Update the bucket name if needed - make sure that you have the adequate rights to put files into that bucket 
 # (check ec2instance role or your laptop local credentials)
-BUCKETNAME="kinesis-datagen"
+BUCKETNAME="gam310-2017"
 
 BASEDIR=$(dirname "$0")
 cd $BASEDIR"/../"
